424ce0ef6e71292f7acd398d8c1506e25a0e45e7
Teks <Teks@TEKS>
2023-11-18 22:11:09 +0700

    Добавьте файлы проекта.

---
+++ laba7.1/laba7.1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+// Функция для приписывания С к числу x
+int f(int x, int c) {
+    int y = x;
+    int b = c;
+    int digit = 0;
+
+    while (b) {
+        b = b / 10;
+        digit++;
+    }
+    return x * pow(10, digit) + c;
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x, c;
+    cout << "Введите x: ";
+    cin >> x;
+    cout << "Введите С: ";
+    cin >> c;
+    int Y = f(x, c);
+    cout << "Y = " << Y << endl;
+
+    return 0;
+}
